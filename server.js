9ded4053f557ace38533700b9aa67954363dff2c
root <root@akarki-pc.localdomain>
2022-11-28 05:18:22 -0500

    url extend and json

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ const args = minimist(process.argv.slice(2));
 const app = express();
 const port = args.port || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.get('/app/', (req, res, next) => {
 	res.status(200).send('OK');
+})
+app.get('/app/roll/', (req, res, next) => {
+	res.send(roll(6, 2, 1));
 })
 
 app.get('*', (req, res) => {
