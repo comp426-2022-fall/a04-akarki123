dc3ea829ffe9658ca7da52cc8b69553f625fb132
root <root@akarki-pc.localdomain>
2022-11-28 05:01:24 -0500

    first two endpoints

--- server.js
+++ server.js
@@ -7,3 +7,11 @@ import { roll } from './lib/roll.js';
 const args = minimist(process.argv.slice(2));
 const app = express();
 const port = args.port || 5000;
+
+app.get('/app/', (req, res, next) => {
+	res.status(200).send('OK');
+})
+
+app.get('*', (req, res) => {
+	res.status(404).send('NOT FOUND');
+})
