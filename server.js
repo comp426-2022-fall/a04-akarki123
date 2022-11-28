f024fb5e5aac72d493f07fa5c427a5377a3152ef
root <root@akarki-pc.localdomain>
2022-11-28 05:30:06 -0500

    did all endpoints

--- server.js
+++ server.js
@@ -17,6 +17,18 @@ app.get('/app/', (req, res, next) => {
 app.get('/app/roll/', (req, res, next) => {
 	res.send(roll(6, 2, 1));
 })
+app.post('/app/roll/', (req, res, next) => {
+	res.send(roll(req.body.sides, req.body.dice, req.body.rolls));
+})
+app.get('/app/roll/:sides/', (req, res, next) => {
+	res.send(roll(req.params.sides, 2, 1));
+})
+app.get('/app/roll/:sides/:dice/', (req, res, next) => {
+	res.send(roll(req.params.sides, req.params.dice, 1));
+})
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res, next) => {
+	res.send(roll(req.params.sides, req.params.dice, req.params.rolls));
+})
 
 app.get('*', (req, res) => {
 	res.status(404).send('NOT FOUND');
