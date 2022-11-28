8e5f241f746907f9c5c2c3127e87c24dae1ec648
root <root@akarki-pc.localdomain>
2022-11-28 05:41:12 -0500

    added listen

--- server.js
+++ server.js
@@ -13,23 +13,25 @@ app.use(express.urlencoded());
 
 app.get('/app/', (req, res, next) => {
 	res.status(200).send('OK');
-})
+});
 app.get('/app/roll/', (req, res, next) => {
 	res.status(200).send(roll(6, 2, 1));
-})
+});
 app.post('/app/roll/', (req, res, next) => {
 	res.status(200).send(roll(req.body.sides, req.body.dice, req.body.rolls));
-})
+});
 app.get('/app/roll/:sides/', (req, res, next) => {
 	res.status(200).send(roll(req.params.sides, 2, 1));
-})
+});
 app.get('/app/roll/:sides/:dice/', (req, res, next) => {
 	res.status(200).send(roll(req.params.sides, req.params.dice, 1));
-})
+});
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res, next) => {
 	res.status(200)send(roll(req.params.sides, req.params.dice, req.params.rolls));
-})
+});
 
 app.get('*', (req, res) => {
 	res.status(404).send('NOT FOUND');
-})
+});
+
+app.listen(port);
