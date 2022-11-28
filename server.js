51f875ab8f0c50356b98914812f1aedd48dde5ee
root <root@akarki-pc.localdomain>
2022-11-28 12:00:43 -0500

    fixed string

--- server.js
+++ server.js
@@ -18,16 +18,16 @@ app.get('/app/roll/', (req, res, next) => {
 	res.status(200).send(roll(6, 2, 1));
 });
 app.post('/app/roll/', (req, res, next) => {
-	res.status(200).send(roll(req.body.sides, req.body.dice, req.body.rolls));
+	res.status(200).send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)));
 });
 app.get('/app/roll/:sides/', (req, res, next) => {
-	res.status(200).send(roll(req.params.sides, 2, 1));
+	res.status(200).send(roll(parseInt(req.params.sides), 2, 1));
 });
 app.get('/app/roll/:sides/:dice/', (req, res, next) => {
-	res.status(200).send(roll(req.params.sides, req.params.dice, 1));
+	res.status(200).send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
 });
 app.get('/app/roll/:sides/:dice/:rolls/', (req, res, next) => {
-	res.status(200).send(roll(req.params.sides, req.params.dice, req.params.rolls));
+	res.status(200).send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
 });
 
 app.get('*', (req, res) => {
