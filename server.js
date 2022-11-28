b8baed4a1806e90b09f809837f69225a0e24076a
root <root@akarki-pc.localdomain>
2022-11-28 03:51:49 -0500

    import modules and create them

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+#!/usr/bin/env node
 
+import express from 'express';
+import minimist from 'minimist';
+import { roll } from './lib/roll.js';
+
+const args = minimist(process.argv.slice(2));
+const app = express();
+const port = args.port || 5000;
